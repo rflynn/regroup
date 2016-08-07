08eb7c3a9aee6d54567dc895c8b8140a6390e6b8
Ryan Flynn <parseerror@gmail.com>
2016-08-07 13:31:45 -0400

    checkpoint tests passing

--- regroup/__init__.py
+++ regroup/__init__.py
@@ -1,5 +1,5 @@
 
-from collections import Counter
+from collections import Counter, defaultdict
 from functools import reduce
 from itertools import groupby
 import re
@@ -22,54 +22,176 @@ Patt.all = [
 
 # NOTE: aha, use a trie of regex matches
 
-def make_trie(words):
+def trie_make(words):
     root = dict()
     for word in words:
         current_dict = root
         for letter in word:
             current_dict = current_dict.setdefault(letter, {})
-        current_dict[None] = None
+        current_dict[''] = {}
     return root
 
 
-class Pattern:
+def dawg_make(d):
+    if len(d) == 1:
+        k, v = list(d.items())[0]
+        v = dawg_make(v)
+        if v and isinstance(v, dict) and len(v) == 1:
+            k2, v2 = list(v.items())[0]
+            if k2:
+                return {k + k2: v2}
+        return d
+    else:
+        return {k: dawg_make(v) if v else v
+                    for k, v in d.items()}
 
-    def __init__(self, s):
-        self.s = s
-        # matrix of all patterns that match each character
-        self.chars = set(s)
-        self.charmatches = [(p, [bool(re.match(p, c)) for c in s])
-                                for flag, p in Patt.all]
-        '''
-        self.seqs = [(p, [(x, len(list(y)))
-                        for x, y in groupby(bools)])
-                            for p, bools in self.charmatches]
-        '''
-        self.seqs = self.charmatches
-        # favor fewer true/false sequences
-        self.seqs = sorted(self.seqs, key=lambda x: len(x[1]))
 
-        self.pattern = self.get_pattern()
 
-    def get_pattern(self):
-        '''for each character, use the first pattern that matches is'''
-        patterns = []
-        for i, c in enumerate(self.s):
-            p = re.escape(c)
-            for pat, seq in self.seqs:
-                if seq[i]:
-                    p = pat
-                    break
-            patterns.append(p)
+def all_len1(l):
+    return all(len(k) == 1 for k in l)
 
-        groups = [(k, len(list(v))) for k, v in groupby(patterns)]
-        grouped = ['{}{{{}}}'.format(k, v).replace('{1}', '')
-                        for k, v in groups]
-        return ''.join(grouped)
 
-    def __repr__(self):
-        return 's={} pattern={} seqs={}'.format(
-            repr(self.s), repr(self.pattern), repr(self.seqs))
+def all_values_not(d):
+    return all(not v or v == {'': {}} for v in d.values())
+
+
+def is_char_class(d):
+    return (all_len1(d.keys())
+            and all_values_not(d))
+
+
+def all_suffixes_identical(d):
+    vals = list(d.values())
+    return len(vals) > 1 and len(set(map(str, vals))) == 1
+
+
+def is_optional(d):
+    items = sorted(list(d.items()))
+    if len(items) == 2:
+        print('items', items)
+        return not items[0][0] and not items[1][1] or items[1][1] == {'': {}}
+    return False
+
+
+def all_len01(l):
+    return set(map(len, l)) == {0, 1}
+
+
+def is_optional_char_class(d):
+    return (all_len01(d.keys())
+            and all_values_not(d))
+
+
+def condense_range(digits):
+    digits = sorted(int(d) for d in digits if d)
+    l = []
+    while digits:
+        # print('digits', digits)
+        i = 1
+        while i < len(digits):
+            if digits[i] != digits[i - 1] + 1:
+                break
+            i += 1
+        if i <= 1:
+            l.append(str(digits[0]))
+        elif i == 2:
+            l.append('{}{}'.format(digits[0], digits[1]))
+        else:
+            l.append('{}-{}'.format(digits[0], digits[i - 1]))
+        if i == len(digits):
+            digits = []
+        else:
+            del digits[:i]
+    return ''.join(l)
+
+
+def suffixes(d):
+    # match up keys with same values
+    return sorted(((k, [a for a, _ in v])
+                    for k, v in groupby(sorted(d.items(),
+                                               key=lambda x: repr(x[0])),
+                                        key=lambda x: x[1])),
+                   key=lambda x: (repr(x[1]), repr(x[0])))
+
+
+def as_charclass(l):
+    # s = ''.join(sorted(l))
+    s = condense_range(l)
+    if len(l) > 1:
+        s = '[' + s + ']'
+    return s
+
+
+def as_opt_charclass(l):
+    s = ''.join(sorted(l))
+    if len(l) > 1:
+        s = '[' + s + ']'
+    s += '?'
+    return s
+
+
+def as_group(l):
+    l = list(l)
+    s = '|'.join(sorted(l))
+    if len(l) > 1:
+        s = '(' + s + ')'
+    return s
+
+
+def repr_keys(l):
+    if all_len1(l):
+        return as_charclass(l)
+    if all_len01(l):
+        return as_opt_charclass(l)
+    return as_group(l)
+
+
+def group(s):
+    if '|' in s:
+        s = '(' + s + ')'
+    return s
+
+
+def dawg_dump(d):
+    # print('dawg_dump', d)
+    if d and len(d) > 1 and is_char_class(d):
+        s = '[' + ''.join(sorted(d.keys())) + ']'
+    elif d and all_suffixes_identical(d):
+        print('all_suffixes_identical', d)
+        # condense suffixes from multiple keys within a subtree
+        v = list(d.values())[0]
+        if len(d) > 1 and all_len1(d):
+            s = '[' + ''.join(sorted(d.keys())) + ']'
+        elif is_optional(d):
+            print('is_optional', d)
+            s = re.escape(sorted(list(d.keys()))[1]) + '?'
+        elif is_optional_char_class(d):
+            s = '[' + ''.join(sorted(d.keys())) + ']?'
+        else:
+            s = '|'.join(k for k in sorted(d.keys()))
+            if len(d) > 1:
+                s = '(' + s + ')'
+        s += dawg_dump(v)
+    elif is_optional(d):
+        print('is_optional', d)
+        s = re.escape(sorted(list(d.keys()))[1]) + '?'
+    elif is_optional_char_class(d):
+        s = '[' + condense_range(d.keys()) + ']?'
+    else:
+        bysuff = suffixes(d)
+        print('suffixes', bysuff)
+        if len(bysuff) < len(d):
+            # at least one suffix shared
+            s = '|'.join(group(repr_keys(k) + dawg_dump(v))
+                            for v, k in bysuff)
+            if len(bysuff) > 1:
+                s = '(' + s + ')'
+        else:
+            s = '|'.join(k + (dawg_dump(v) if v else '')
+                                for k, v in sorted(d.items()))
+            if len(d) > 1:
+                s = '(' + s + ')'
+    return s
 
 
 def match(values):
@@ -77,21 +199,11 @@ def match(values):
     if not values:
         return None
 
-    valcnts = Counter(values)
-    if len(valcnts) == 1:
-        return list(valcnts.keys())[0]
+    print('values', values)
+    print('trie', trie_make(values))
+    print('dawg', dawg_make(trie_make(values)))
+    # print('dawg2', dawg_dump(trie_make(values)))
+    print('dawg3', dawg_dump(dawg_make(trie_make(values))))
 
-    chars = {k: Pattern(k) for k in valcnts.keys()}
+    return dawg_dump(dawg_make(trie_make(values)))
 
-    from pprint import pprint
-    # pprint(chars)
-
-    patterns = [v.pattern for v in chars.values()]
-    patset = sorted(set(patterns))
-
-    print(make_trie(values))
-
-    if len(patset) == 1:
-        return patset[0]
-
-    return '(' + '|'.join(patset) + ')'
--- tests/test_init.py
+++ tests/test_init.py
@@ -1,8 +1,20 @@
 
+import re
 import unittest
 from unittest import skip
 
-from regroup import match
+from regroup import match, dawg_make
+
+
+class TestDAWG(unittest.TestCase):
+
+    def test_dawg1(self):
+        self.assertEqual(dawg_make({'2': {'3': {'': {}}}}),
+                         {'23': {'': {}}})
+
+    def test_dawg1(self):
+        self.assertEqual(dawg_make({'1': {'2': {'3': {'': {}}}}}),
+                         {'123': {'': {}}})
 
 
 class TestDigits(unittest.TestCase):
@@ -17,23 +29,78 @@ class TestDigits(unittest.TestCase):
         self.assertEqual('0', match(['0', '0']))
 
     def test_01(self):
-        self.assertEqual('[0-9]{2}', match(['00', '11']))
+        self.assertEqual('(00|11)', match(['00', '11']))
 
     def test_0_1(self):
-        self.assertEqual('[0-9] [0-9]', match(['0 0', '1 1']))
+        self.assertEqual('(0 0|1 1)', match(['0 0', '1 1']))
 
     def test_float_maybe_dot(self):
-        self.assertEqual('([0-9]|[0-9]\.)', match(['0', '0.']))
+        # self.assertEqual('([0-9]|[0-9]\.)', match(['0', '0.']))
+        self.assertEqual('0\.?', match(['0', '0.']))
 
     def test_float_maybe_(self):
-        self.assertEqual('([0-9]|[0-9]\.[0-9])',
+        # self.assertEqual('([0-9]|[0-9]\.[0-9])',
+        #                match(['0', '0.1']))
+        self.assertEqual('0(|.1)',
                          match(['0', '0.1']))
 
-    @skip('')
+    def test_float_variable_prefix(self):
+        self.assertEqual('0(|.12?)',
+                         match(['0', '0.1', '0.12']))
+
+
+    def test_float_variable_prefix23(self):
+        # 0(|.1(|23))
+        # 0(.1(23)?)?
+        self.assertEqual('0(|.1(|23))',
+                         match(['0', '0.1', '0.123']))
+
     def test_numbers(self):
-        self.assertEqual('([0-9]|[0-9]{2}|[0-9]{3})',
-                         match(list(map(str, range(101)))))
+        # (100?|[1-9]|)|[2-9][0-9]?|0
+        # (0|(1(|00?|[1-9]))|[2-9][0-9]?)
+        # (0|(1(|00?|[1-9]))|[2-9][0-9]?)
+
+        # (100?|[1-9]|)|[02-9][0-9]?
+        # (100|[0-9][0-9]?)
+        # (1?[0-9][0-9]?)  XXX: too broad
+        vals = list(map(str, range(101)))
+        pattern = match(vals)
+        fullpat = '^({})$'.format(pattern)
+        self.assertTrue(all(re.match(fullpat, v) for v in vals))
+        self.assertEqual('(0|(1(|00?|[1-9]))|[2-9][0-9]?)', pattern)
 
     def test_hundreds(self):
         self.assertEqual('[12]00',
                          match(['100', '200']))
+
+
+class TestStrings(unittest.TestCase):
+
+    '''
+
+    def test_realworld_1(self):
+        # ref: http://stackoverflow.com/questions/1410822/how-can-i-detect-common-substrings-in-a-list-of-strings
+        self.assertEqual(
+            match(['EFgreen',
+                   'EFgrey',
+                   'EntireS1',
+                   'EntireS2',
+                   'J27RedP1',
+                   'J27GreenP1',
+                   'J27RedP2',
+                   'J27GreenP2',
+                   'JournalP1Black',
+                   'JournalP1Blue',
+                   'JournalP1Green',
+                   #'JournalP1Red',
+                   'JournalP2Black',
+                   'JournalP2Blue',
+                   'JournalP2Green']),
+             '(E(Fgre(en|y)|ntireS[12])|J(27(GreenP[12]|RedP[12])|ournalP[12](Bl(ack|ue)|Green)))')
+            ['EF(gre(en|y))'
+             'EntireS[12]'
+             'J27(Red|Green)P[12]'
+             'JournalP[12](Red|Green|Blue)'])
+
+    '''
+
