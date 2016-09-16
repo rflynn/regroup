7c9e16d95a8f3549d88347315e3d3cc7c9041bd6
Ryan Flynn <parseerror+github@gmail.com>
2016-09-16 16:20:55 -0400

    implement ./regroup.py --count

--- regroup.py
+++ regroup.py
@@ -11,6 +11,7 @@ if __name__ == '__main__':
     # ./regroup.py --relax --cluster-prefix-len=2
 
     import argparse
+    import re
     import sys
 
     # commandline options
@@ -19,20 +20,29 @@ if __name__ == '__main__':
                         help='attempt to simplify pattern where possible')
     parser.add_argument('--cluster-prefix-len', type=int,
                         help='split by prefix of a given length')
+    parser.add_argument('--count', action='store_true',
+                        help='count matches against input')
     args = parser.parse_args()
 
     # run
-    lines = (line.rstrip('\r\n') for line in sys.stdin.readlines())
+    lines = [line.rstrip('\r\n') for line in sys.stdin.readlines()]
     dawg = DAWG.from_iter(lines)
 
     if args.relax:
         dawg = DAWGRelaxer(dawg).relax()
 
     # output
+    # either we split/cluster one big pattern into sub-patterns by some method...
+    # ...or we just dump one big pattern
     if args.cluster_prefix_len:
         clusters = dawg.cluster_by_prefixlen(args.cluster_prefix_len)
         for prefix, suffix_tree in clusters:
             d = DAWG.from_dawg(suffix_tree)
-            print(prefix + DAWGRelaxer(d).relax().serialize())
+            pattern = prefix + DAWGRelaxer(d).relax().serialize()
+            if args.count:
+                cnt = sum(re.match('^' + pattern + '$', line) is not None for line in lines)
+                print(cnt, pattern)
+            else:
+                print(pattern)
     else:
         print(dawg.serialize())
